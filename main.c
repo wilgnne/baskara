ae9bc24c7a5f732cb150ba6f69aa2239f9e242c6
Wilgnne Khawan <wilgnne.kba@gmail.com>
2022-07-13 15:04:35 -0300

    finalizei

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include "stdio.h"
+#include "math.h"
 
 // Faça um programa que leia um número real x e calcule o valor de f (x) = √x + (x/2) + xx.
 // (pesquise sobre as funções sqrt e pow ).
 
 int main()
 {
+    float x, f;
+    printf("Digite um numero real: ");
+    scanf("%f", &x);
+
+    f = sqrt(x) + (x / 2) + pow(x, 2);
+
+    printf("O valor de f(x) = %.2f\n", f);
+
     return 0;
 }
