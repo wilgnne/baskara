89d3e26cb423bcc9e9e79229946787880a3649d0
Wilgnne Khawan <wilgnne.kba@gmail.com>
2022-07-13 15:02:31 -0300

    base do programa

--- main.c
+++ main.c
@@ -1,2 +1,9 @@
+#include "stdio.h"
+
 // Faça um programa que leia um número real x e calcule o valor de f (x) = √x + (x/2) + xx.
 // (pesquise sobre as funções sqrt e pow ).
+
+int main()
+{
+    return 0;
+}
